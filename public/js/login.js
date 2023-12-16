57c8e90da0772e55f666822c4afa748cb5ec0493
Michael Hong <michaelhong0918@gmail.com>
2023-12-16 15:54:26 +0000

    Add login and logout viewer

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var form = document.querySelector('.needs-validation')
+    form.addEventListener('submit', function (event) {
+        event.preventDefault()
+        event.stopPropagation()
+        form.classList.add('was-validated')
+
+        if (form.checkValidity()) {
+            $.ajax({
+                type: "POST",
+                url: '/api/login',
+                data: {
+                    username: $('#username').val(),
+                    email: $('#email').val(),
+                    password: $('#password').val(),
+                },
+                success: () => {
+                    window.location.href = "/employee"
+                },
+            })
+        }
+    }, false)
+});
