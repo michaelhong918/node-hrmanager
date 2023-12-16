17b746d95b3493f340d7ad015fc21fd0fb5f89be
Michael Hong <michaelhong0918@gmail.com>
2023-12-16 14:36:13 +0000

    Add create and post action to employeejs

--- public/js/employee.js
+++ public/js/employee.js
@@ -4,4 +4,32 @@ $(document).ready(function () {
     function onCancel() {
         window.location.href = "/employee"
     }
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var form = document.querySelector('.needs-validation')
+    form.addEventListener('submit', function (event) {
+        event.preventDefault()
+        event.stopPropagation()
+        form.classList.add('was-validated')
+
+        if (form.checkValidity()) {
+            $.ajax({
+                type: form.id ? "PUT" : "POST",
+                url: `/api/employees${form.id && "/" + form.id}`,
+                data: {
+                    email: $('#email').val(),
+                    address: $('#address').val(),
+                    salary: $('#salary').val(),
+                    position: $('#position').val(),
+                    status: $('#status').val(),
+                    managerId: $('#manager').val(),
+                    hireDate: $('#hireDate').val(),
+                    terminateDate: $('#terminateDate').val(),
+                },
+                success: () => {
+                    window.location.href = "/employee"
+                },
+            })
+        }
+    }, false)
 });
