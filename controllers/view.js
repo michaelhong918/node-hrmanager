307061af2e5d2524a362a5969cd0db27b5d32cb3
Michael Hong <michaelhong0918@gmail.com>
2023-12-16 12:21:29 +0000

    Create view controller

---
+++ controllers/view.js
@@ -0,0 +1,95 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+const router = require('express').Router();
+const moment = require("moment");
+const db = require("../models");
+
+// Routes
+// =============================================================
+
+// Each of the below routes just handles the HTML page that the user gets sent to.
+router.get('/', renderEmployees);
+
+router.get('/employee', renderEmployees);
+router.get('/employee/create', renderEmployeeCreate);
+router.get('/employee/:id', renderEmployeeEdit);
+
+router.get('/manager', renderManagers);
+router.get('/manager/create', renderManagerCreate);
+router.get('/manager/:id', renderManagerEdit);
+
+// Display all employees table
+function renderEmployees(_, res) {
+  db.Employee.findAll({
+    include: [db.Manager],
+    raw: true
+  }).then((employees) => {
+    const data = employees.map(employee => ({
+      ...employee,
+      status: employee.status ? "Regular" : "Otherwise",
+      hireDate: moment(employee.hireDate).format("MMM Do YYYY"),
+      terminateDate: moment(employee.terminateDate).format("MMM Do YYYY")
+    }))
+    res.render('employees', { employees: data })
+  });
+}
+
+// Display employee craete form
+function renderEmployeeCreate(_, res) {
+  db.Manager.findAll().then((managers) => {
+    res.render('employee', { managers });
+  });
+}
+
+// Display employee edit form
+function renderEmployeeEdit(req, res) {
+  db.Employee.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [db.Manager],
+    raw: true
+  }).then((employee) => {
+    db.Manager.findAll().then((managers) => {
+      const data = {
+        ...employee,
+        hireDate: moment(employee.hireDate).format("YYYY-MM-DD"),
+        terminateDate: moment(employee.terminateDate).format("YYYY-MM-DD")
+      }
+      res.render('employee', { employee: data, managers });
+    })
+  });
+}
+
+// Display all managers table
+function renderManagers(_, res) {
+  db.Manager.findAll({ raw: true }).then((managers) => {
+    const data = managers.map(manager => ({
+      ...manager,
+      createdAt: moment(manager.createdAt).format('MMM Do YYYY')
+    }))
+    res.render('managers', { managers: data })
+  });
+}
+
+// Display manager create form
+function renderManagerCreate(_, res) {
+  res.render('manager');
+}
+
+// Display manager edit form
+function renderManagerEdit(req, res) {
+  db.Manager.findOne({
+    where: {
+      id: req.params.id,
+    },
+  }).then((manager) => {
+    res.render('manager', { manager });
+  });
+}
+
+module.exports = router;
