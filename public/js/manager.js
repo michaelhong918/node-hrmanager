598065c7e79aaa7ffad92477f9366ab73e6fe6b2
Michael Hong <michaelhong0918@gmail.com>
2023-12-16 14:36:35 +0000

    Add create and update action to managerjs

--- public/js/manager.js
+++ public/js/manager.js
@@ -4,4 +4,27 @@ $(document).ready(function () {
     function onCancel() {
         window.location.href = "/manager"
     }
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var form = document.querySelector('.needs-validation')
+    form.addEventListener('submit', function (event) {
+        event.preventDefault()
+        event.stopPropagation()
+        form.classList.add('was-validated')
+
+        if (form.checkValidity()) {
+            $.ajax({
+                type: form.id ? "PUT" : "POST",
+                url: `/api/managers${form.id && "/" + form.id}`,
+                data: {
+                    firstname: $('#firstname').val(),
+                    lastname: $('#lastname').val(),
+                    department: $('#department').val(),
+                },
+                success: () => {
+                    window.location.href = "/manager"
+                },
+            })
+        }
+    }, false)
 });
